3af7a47c0bc05a52d62cb0a8572990b546d69cbc
Robert Terry <robert.terry@coramdeoacademy.org>
2017-12-22 14:15:08 -0600

    Added seperate tests

---
+++ Lexer/Tests/RegexLexerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexer.Tests
+{
+#if true
+    class RegexLexerTest
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            return 0;
+        }
+        public bool RunAllTests()
+        {
+            return true;
+        }
+    }
+#endif
+}
