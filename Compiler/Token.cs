b41a19475b1baf5c297b63bd04a9fb40e7815789
Robert Terry <robert.terry@coramdeoacademy.org>
2017-12-21 19:13:16 -0600

    Removed all unnecessary using statements

--- Compiler/Token.cs
+++ Compiler/Token.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lexer
+﻿namespace Lexer
 {
-    
+
     /// <summary>
     /// A parser token
     /// </summary>
