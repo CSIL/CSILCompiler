0a99f4c56cd52d95100ef7fde8d47dda47909d1a
Robert Terry <robert.terry@coramdeoacademy.org>
2017-12-22 13:52:45 -0600

    Added UnitTest

---
+++ LexerTests/Implementations/RegexLexerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lexer.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexer.Implementation.Tests
+{
+    [TestClass()]
+    public class RegexLexerTests
+    {
+        [TestMethod()]
+        public void RegexLexerTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod()]
+        public void RegexLexerTest1()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod()]
+        public void GetAllTokensTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
